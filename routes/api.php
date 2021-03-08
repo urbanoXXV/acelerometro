acc1094f4e7340dfec96fea20f9f2e6e1676761e
Jhony Diaz <urbanodiazq@gmail.com>
2021-03-07 20:41:20 -0500

    ultimos cambios

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SetDataController;
+use App\Http\Controllers\Api\GetDataController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,7 @@ use App\Http\Controllers\Api\SetDataController;
 |
 */
 
+Route::apiResource('GetData', GetDataController::class);
 Route::apiResource('SetData', SetDataController::class);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
